823b12a6c2b4a42d3be8431c2b435f40c688186c
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2026-06-04 16:50:33 -0400

    bump version to 0.2.0

--- wp-stellate.php
+++ wp-stellate.php
@@ -7,7 +7,7 @@
  * Description: Stellate for your WordPress GraphQL API
  * Author: Stellate
  * Author URI: https://stellate.co
- * Version: 0.1.8
+ * Version: 0.2.0
  * Requires at least: 5.0
  * Tested up to: 6.4.0
  * Requires PHP: 7.1
@@ -16,7 +16,7 @@
  *
  * @package  Stellate
  * @author   Stellate
- * @version  0.1.8
+ * @version  0.2.0
  */
 
 /**
